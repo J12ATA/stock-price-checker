298ffb005390e7bbd722d1fde932bdb88c61242e
Glitch (fcc-projects-stock-price-checker) <none>
2019-04-18 22:45:47 +0000

    🚖☄️ Updated with Glitch

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,7 +8,7 @@
 
 const chaiHttp = require('chai-http');
 const chai = require('chai');
-const assert = chai.assert;
+const { assert } = chai;
 const server = require('../server');
 
 chai.use(chaiHttp);
